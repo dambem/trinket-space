8a5228364fc33dfa126825d6f5c6449630352c04
Damian B <bembendamian@gmail.com>
2024-10-19 22:38:41 +0100

    Testing this

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -1,44 +1,48 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { CustomMDX } from "app/components/mdx";
+import { formatDate, getBlogPosts } from "app/lib/posts";
 import { metaData } from "app/config";
-import { defineQuery } from "next-sanity";
+import { defineQuery, PortableText, type SanityDocument } from "next-sanity";
+
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "app/sanity/client";
 import Link from "next/link";
 import Image from "next/image";
-
 const options = { next: { revalidate: 60 } };
 
-const POST_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
-  _id,
-  name,
-  slug,
-  content,
-  publishedAt,
-  summary,
-  image
-}`);
 
-const ALL_SLUGS_QUERY = defineQuery(`*[_type == "post" && defined(slug.current)]{
-  "slug": slug.current
-}`);
+const EVENT_QUERY = defineQuery(`*[
+  _type == "post" &&
+  slug.current == $slug
+][0]`);
+const { projectId, dataset } = client.config();
+
 
 export async function generateStaticParams() {
-  const slugs = await client.fetch(ALL_SLUGS_QUERY);
-  return slugs.map((slug) => ({ slug: slug.slug }));
+  let posts = getBlogPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 }
 
 export async function generateMetadata({
   params,
 }): Promise<Metadata | undefined> {
-  const post = await client.fetch(POST_QUERY, { slug: params.slug }, options);
+  let post = getBlogPosts().find((post) => post.slug === params.slug);
   if (!post) {
     return;
   }
 
-  const { name: title, publishedAt, summary: description, image } = post;
-  const ogImage = image
-    ? image.url
+  let {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+  } = post.metadata;
+  let ogImage = image
+    ? image
     : `${metaData.baseUrl}/og?title=${encodeURIComponent(title)}`;
 
   return {
@@ -48,8 +52,8 @@ export async function generateMetadata({
       title,
       description,
       type: "article",
-      publishedTime: publishedAt,
-      url: `${metaData.baseUrl}/blog/${params.slug}`,
+      publishedTime,
+      url: `${metaData.baseUrl}/blog/${post.slug}`,
       images: [
         {
           url: ogImage,
@@ -65,18 +69,23 @@ export async function generateMetadata({
   };
 }
 
-export default async function Blog({ params }: { params: { slug: string } }) {
-  const post = await client.fetch(POST_QUERY, { slug: params.slug }, options);
-  if (!post) {
+export default async function Blog({ params }: {params: {slug: string}}) {
+  const event = await client.fetch(EVENT_QUERY, params, options);
+  if (!event) {
     notFound();
   }
-
+  const {
+    post,
+    name,
+    slug
+  } = event;
   return (
     <main>
-      <h1>{post.name}</h1>
-      <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
-      <article className="prose prose-quoteless prose-neutral dark:prose-invert">
-        <CustomMDX source={post.content} />
+    <p>
+      {name}
+    </p>
+    <article className="prose prose-quoteless prose-neutral dark:prose-invert">
+        <CustomMDX source={post} />
       </article>
     </main>
   );
